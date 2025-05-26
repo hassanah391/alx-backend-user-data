0b9a1b7154f968442cfdc4cc6e0d6957b995faa0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-26 20:13:19 +0300

    Adds filter_datum function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module filtered_logger """
+import logging
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    """returns the log message obfuscated"""
+    for field in fields:
+        match = re.search("{}=([^{}]+)".format(field, separator), message)
+        if match:
+            message = re.sub("{}".format(match.group(1)), redaction, message)
+    return message
