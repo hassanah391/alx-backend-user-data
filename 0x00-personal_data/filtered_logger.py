c59bc33d922fbb2d399c1d945f53128d4e931f67
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-26 20:20:05 +0300

    Annotates return-type and follow pep8

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,10 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(
+    fields: List[str], redaction: str,
+        message: str, separator: str
+        ) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         match = re.search("{}=([^{}]+)".format(field, separator), message)
