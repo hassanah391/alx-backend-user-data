b551e36ef4733ffc44fe2f0f429c398db0d84fad
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-27 17:21:32 +0300

    Adds RedactingFormatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,3 +16,25 @@ def filter_datum(
                 re.escape(separator)),
         lambda m: m.group(1) + '=' + redaction, message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Takes record message and obfuscate it using filter_datum()
+            then log the obfuscated mesage with a specific format
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+            )
+        return super().format(record)
