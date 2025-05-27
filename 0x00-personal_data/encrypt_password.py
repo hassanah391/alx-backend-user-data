c0786306086672f8133bbd4f3cfbd38e0bbe9891
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-28 01:45:46 +0300

    Add is_valid()

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,10 @@ def hash_password(password: str) -> bytes:
     """returns a salted, hashed password"""
     hased = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hased
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid or not
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
