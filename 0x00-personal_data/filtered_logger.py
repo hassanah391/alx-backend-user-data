b7226746181e7a6d4698c365f7bd14cb477f4e08
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-27 20:22:19 +0300

    Adds get_db()

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import logging
 from typing import List
 import re
+import os
+import mysql.connector
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -57,3 +59,17 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db():
+    """returns a connector to the database"""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', 'my_db')
+    connector = mysql.connector.connect(
+        host= host,
+        password=password,
+        user=username,
+        database=db_name)
+    return connector
