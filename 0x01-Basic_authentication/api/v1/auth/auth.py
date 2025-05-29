8e5a4af02a8008b0e18b51631be8f01a6389abf5
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-05-29 17:55:04 +0300

    Documents auth module

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,18 +5,18 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """"""
+    """ Auth class for authentication system"""
     def require_auth(
         self, path: str,
         excluded_paths: List[str]
     ) -> bool:
-        """"""
+        """Checks If an endpoint require auth"""
         return False
 
     def authorization_header(self, request=None) -> str:
-        """"""
+        """ do know yet """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+        """do know yet"""
         return None
